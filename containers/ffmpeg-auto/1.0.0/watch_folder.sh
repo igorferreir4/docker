6d59090c7eb62b9a5adfbe8b8158a16a0b137220
Igor Ferreira <94236607+igorferreir4@users.noreply.github.com>
2024-05-08 21:17:07 -0300

    add ffmpeg

---
+++ containers/ffmpeg-auto/1.0.0/watch_folder.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Função para converter arquivos MKV para MP4
+convert_files() {
+    for file in "$1"/*.mkv; do
+        ffmpeg -fflags +genpts -i "$file" -y -map 0 -c:V copy -c:a aac -ac 2 -c:s mov_text "${file%.*}.mp4"
+        rm "$file"
+    done
+}
+
+# Função para mover pastas
+move_folders() {
+    for dir in "$1"/*; do
+        if [ -d "$dir" ]; then
+            if [ "$(basename "$dir")" != "series" ] && [ "$(basename "$dir")" != "filmes" ]; then
+                mv "$dir" /saida/$(basename "$1")/
+            fi
+        fi
+    done
+}
+
+# Loop infinito para monitorar a pasta de entrada
+while true; do
+    inotifywait -r -e create --format '%w%f' /entrada/series /entrada/filmes | while read file; do
+        if [ "${file##*.}" = "mkv" ]; then
+            convert_files "$(dirname "$file")"
+            move_folders "$(dirname "$file")"
+        fi
+    done
+done
